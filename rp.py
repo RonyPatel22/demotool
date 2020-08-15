d1d896b1f20ac098ff316a7196cd524405fcd1c3
RonyPatel22 <patelrony646@gmail.com>
2020-08-15 17:03:57 +0530

    rpdemo

---
+++ rp.py
@@ -0,0 +1,29 @@
+name = input("Type your name : ")
+print("Hello " + name )
+
+fathername = input("What is your father name : ")
+print("Your father name is " + fathername )
+
+age = input("What is your age : ")
+print("Your age is " + age )
+
+gender = input("What is your gender male/female : ")
+print("Your gender is " + gender )
+
+graduation = input("What is your graduation 10th/12th : ")
+print("Your graduation is " + graduation + " Pass")
+
+hobby = input("What is your hobby : ")
+print("Your hobby is " + hobby )
+
+food = input("What is your favorite food : ")
+print("Your favorite food is " + food )
+
+(" \t ")
+(" \t ")
+(" \t ")
+(" \t ")
+print("Thank You So Much For Give Your Important Time")
+
+
+print("This tool is Created by \"Techy Rony\" ")
